5e3f1f90aacdc66358c111ff9546240d9081b745
Bruno Valle <brunolv6@gmail.com>
2019-08-29 10:48:45 -0300

    Updates

---
+++ precache-manifest.813dd50f479b227473128b7fb6ab0be0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ef8d08a407f170c8076c3c6630f758",
+    "url": "/project-portfolio/index.html"
+  },
+  {
+    "revision": "942076e45cb77deb3685",
+    "url": "/project-portfolio/static/css/main.12bdcdf1.chunk.css"
+  },
+  {
+    "revision": "62bda684be2af45314c2",
+    "url": "/project-portfolio/static/js/2.90ef0de0.chunk.js"
+  },
+  {
+    "revision": "942076e45cb77deb3685",
+    "url": "/project-portfolio/static/js/main.0e6d9433.chunk.js"
+  },
+  {
+    "revision": "c4589b152830cfa53b4a",
+    "url": "/project-portfolio/static/js/runtime~main.515880ca.js"
+  },
+  {
+    "revision": "9154065b13576dfda8d19de13d28c7fe",
+    "url": "/project-portfolio/static/media/ecommerce.9154065b.png"
+  },
+  {
+    "revision": "d023ec38e17f80abc4b99a5c8a198bd0",
+    "url": "/project-portfolio/static/media/firebase.d023ec38.png"
+  },
+  {
+    "revision": "ce551d75784970b1ad3535e2b25e5179",
+    "url": "/project-portfolio/static/media/fotoPessoal.ce551d75.jpg"
+  },
+  {
+    "revision": "1bb027109345a90a9eab1e929d8669c2",
+    "url": "/project-portfolio/static/media/github.1bb02710.svg"
+  },
+  {
+    "revision": "81c494621e3ec47b5bf4fc875d0c178b",
+    "url": "/project-portfolio/static/media/heroku.81c49462.svg"
+  },
+  {
+    "revision": "f42ca7fb93e8173ce30419583a0e86b3",
+    "url": "/project-portfolio/static/media/job.f42ca7fb.png"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/project-portfolio/static/media/linkedin.5b238434.svg"
+  },
+  {
+    "revision": "3ac9dd2d9a7fc89d477953157feb269a",
+    "url": "/project-portfolio/static/media/react.3ac9dd2d.png"
+  },
+  {
+    "revision": "d073667ec46c3f98b2145ee39209dd56",
+    "url": "/project-portfolio/static/media/redux.d073667e.png"
+  },
+  {
+    "revision": "e4590c1a8965385e21eac0f0bff9c585",
+    "url": "/project-portfolio/static/media/stripe.e4590c1a.svg"
+  }
+]);
